8b2de432e344c13a0bb6f21c777dba8e96992679
anyakul <anyakulikova111@gmail.com>
2020-02-20 00:36:02 +0300

    Делаю якорную ссылку получить бесплатную консультацию

--- source/js/about-company-script.js
+++ source/js/about-company-script.js
@@ -2,7 +2,7 @@
 
 var openButton = document.querySelector('.about-company__button');
 var text = document.querySelector('.about-company__text');
-var hiddenText = text.querySelector('.about-company__hidden-text');
+var hiddenText = document.querySelector('.about-company__hidden-text');
 
 openButton.addEventListener('click', function () {
   hiddenText.classList.remove('visually-hidden');
--- source/js/modal-script.js
+++ source/js/modal-script.js
@@ -5,6 +5,34 @@ var closeButton = document.querySelector('.popup__button');
 var popup = document.querySelector('.popup');
 var sendButton = document.querySelector('.popup-form');
 var userName = popup.querySelector('[name=user-name]');
+var phone = popup.querySelector("[name=number]");
+var question = popup.querySelector("[name=question]");
+
+var isStorageSupport = true;
+var storage = "";
+  
+try {
+   storage = localStorage.getItem("user-name");    
+} catch (err) {
+   isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();	 
+  popup.classList.add("modal-show");
+    
+  if (storage) {
+    userName.value = localStorage.getItem("userName");
+	  phone.value = localStorage.getItem("phone");
+    if (userName.value) {
+	    phone.focus();
+  	} else {
+	    question.focus();
+	  }
+  } else {
+    namem.focus();
+  }    
+);
 
 var closePopup = function () {
   popup.classList.remove('popup--show');
@@ -21,6 +49,18 @@ closeButton.addEventListener('click', function () {
 
 sendButton.addEventListener('submit', function (evt) {
   evt.preventDefault();
+ /* if (!userName.value  || !phone.value || !question.value) {
+	  popup.classList.remove("modal-error");
+	  popup.offsetWidth = popup.offsetWidth; 
+    popup.classList.add("modal-error");
+  } else {*/
+      if (isStorageSupport) { 
+        localStorage.setItem("user-name", userName.value);
+		    localStorage.setItem("phone", phone.value);
+	    	localStorage.setItem("question", question.value);
+      }
+   // }
+  
   closePopup();
 });
 
