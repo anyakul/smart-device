827260e04463dfb02fdebca69ed0e2ee6e4527ec
anyakul <anyakulikova111@gmail.com>
2020-02-21 14:38:42 +0300

    делаю открытие и закрытие формы отправки вопроса, автоматическое обновление js файлов

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch('source/js/**/*', gulp.series(gulp.series(["js"])));
 });
 
 gulp.task("refresh", function (done) {
@@ -79,6 +80,11 @@ gulp.task("html", function () {
       include()
     ]))
     .pipe(gulp.dest("build"));
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("copy", function () {
--- source/js/modal-script.js
+++ source/js/modal-script.js
@@ -1,121 +1,79 @@
-'use strict';
-
-var link = document.querySelector('.contacts__button');
-var closeButton = document.querySelector('.popup__button');
-var popup = document.querySelector('.popup');
-var sendButton = document.querySelector('.popup-form');
-var popupMistake = document.querySelector('.popup-mistake');
-var popupSuccess = document.querySelector('.popup-success');
-var userName = popup.querySelector('[name=user-name]');
-var phone = popup.querySelector("[name=number]");
-var question = popup.querySelector("[name=question]");
-var closeMistakeButton = document.querySelector('.popup-mistake__close-button');
-var popupSuccess = document.querySelector('.popup-success');
-var closeSuccessButton = popupSuccess.querySelector('.popup-success__close-button');
-var successButton = popupSuccess.querySelector('.popup-success__button');
-var mistakeButton = popupMistake.querySelector('.popup-mistake__button');
+var link = document.querySelector(".contacts__button");
+  
+var popup = document.querySelector(".popup");
+var popupActive = document.querySelector(".popup--show");
+var close = popup.querySelector(".popup__close"); 
+var form = popup.querySelector("form");
+var namem = popup.querySelector("[name=namem]");
+var email = popup.querySelector("[name=email]");
+var message = popup.querySelector("[name=message]");
+  
 var isStorageSupport = true;
 var storage = "";
   
 try {
-   storage = localStorage.getItem("user-name");    
+   storage = localStorage.getItem("namem");    
 } catch (err) {
    isStorageSupport = false;
-}
+}  
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();	 
   popup.classList.add("popup--show");
-    
-  if (storage) {
-    userName.value = localStorage.getItem("userName");
-	  phone.value = localStorage.getItem("phone");
-    if (userName.value) {
-	    phone.focus();
-  	} else {
-	    question.focus();
-	  }
-  } else {
-    userName.focus();
-  }    
-});
-
-var closePopup = function () {
-  popup.classList.remove('popup--show');
-};
-
-link.addEventListener('click', function () {
-  popup.classList.add('popup--show');
-  userName.focus();
   addEventListeners();
 });
-
-closeButton.addEventListener('click', function () {
-  closePopup();
+     /*
+    if (storage) {
+          namem.value = localStorage.getItem("namem");
+	      email.value = localStorage.getItem("email");
+          if (email.value) {
+			  message.focus();
+			  
+		  } else {
+			  email.focus();
+		  }
+    } else {
+      namem.focus();
+    }    
+ });
+  */
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();	
+    popup.classList.remove("popup--show");
 });
 
 var addEventListeners = function() {
-  document.addEventListener('click', function (evt) {
-    if (evt.target !== popup && !popup.contains(evt.target)) {
-      closePopup();
-    }
-  })
-};
-
-var showMistakeMessage = function() {
-  popupMistake.classList.add("popup-mistake--show");
-  closeMistakeButton.addEventListener('click', function () {
-    popupMistake.classList.remove('popup-mistake--show');
-  });
-  
-  mistakeButton.addEventListener('click', function () {
-    popupMistake.classList.remove('popup-mistake--show');
-    popup.classList.add("popup--show");
+  document.addEventListener("mouseup", function (evt) {
+    evt.preventDefault();
+  //  if (evt.target === popup) {                    
+          popup.classList.remove("popup--show");
+  //  }
   });
 }
 
-sendButton.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-  var phone = validPhone();
-  if (!userName.value || !phone.value) {
-    closePopup();
-	  showMistakeMessage();
-  }
-  else if (userName.value || phone.value) {
-   /* if (!phone) {
-      showMistakeMessage();
-    }
-    /*else if (isStorageSupport) { 
-      localStorage.setItem("user-name", userName.value);
-      localStorage.setItem("phone", phone.value);
-      localStorage.setItem("question", question.value);*/
-      closePopup();
-      popupSuccess.classList.add('popup-success--show');
-      closeSuccessButton.addEventListener('click', function () {
-        popupSuccess.classList.remove('popup-success--show');
-      })
-      successButton.addEventListener('click', function () {
-        popupSuccess.classList.remove('popup-success--show');
-      })
-  //  }
-  }
-})
 
-function validPhone() {
-    var re = /^\d[\d\(\)\ -]{4,14}\d$/;
-    var myPhone = phone.value;
-    var valid = re.test(myPhone);
-    if (valid) {
-      return true
-    };
-    return false;
-}  
-
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains('popup--show')) {
-      closePopup();
+/*
+form.addEventListener("submit", function (evt) {
+  if (!namem.value  || !email.value || !message.value) {
+      evt.preventDefault();
+	  popup.classList.remove("modal-error");
+	  popup.offsetWidth = popup.offsetWidth; 
+      popup.classList.add("modal-error");
+  } else {
+      if (isStorageSupport) { 
+        localStorage.setItem("namem", namem.value);
+		localStorage.setItem("email", email.value);
+	//	localStorage.setItem("message", message.value);
+      }
     }
-  }
-});
+}); 
+ */
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("popup--show")) {
+        popup.classList.remove("popup--show");
+      }
+    }
+}); 
+
